58487c7eef49a31903958ba38dc05d3ebd7e758d
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-04-30 23:26:18 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  let arr = []
+  if (a == 0)
+    return false
+  let dis = b ** 2 - 4 * a * c;
+  console.log('D = ' + dis);
+  if (dis < 0) 
+    return false;
+  if (dis == 0) 
+    arr.push(-b / (2 * a));
+  if (dis > 0) {
+    arr.push((-b + Math.sqrt(dis)) / (2 * a));
+    arr.push((-b - Math.sqrt(dis)) / (2 * a));
+  }
   return arr;
-}
+};
+console.log(solveEquation(5,15,7));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  let mPercent = (percent / 100) / 12; 
+  let bodyCredit = amount - contribution;
+  let mPayment = bodyCredit * (mPercent + (mPercent / (((1 + mPercent) ** countMonths) - 1)));
+  let tAmount = mPayment * countMonths;
+  let total = tAmount.toFixed(2);
+  return total;
+};
+
+console.log(calculateTotalMortgage(15,0,10000,36));
+
+
