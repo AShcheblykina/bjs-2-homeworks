54b390bd6131303fad3fd76109546829d0460c93
AShcheblykina <116199504+AShcheblykina@users.noreply.github.com>
2023-05-07 22:39:04 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -30,19 +30,20 @@ function summElementsWorker(...arr) {
 
 
 function differenceMaxMinWorker(...arr) {
+	if (arr.length === 0) return 0;
 
-	 let min = arr[0];
-	 let max = arr[0];
+	let max = arr[0];
+	let min = arr[0];
 
 	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] < min) {
-      min = arr[i];
-    }
 		if (arr[i] > max) {
-       max = arr[i];
-    }
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
 	}
-	 return max - min;
+	return max - min;
 }
 
 
@@ -53,10 +54,10 @@ function differenceEvenOddWorker(...arr) {
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] % 2 !== 0) {
 			sumEvenElement += arr[i];
-    }
+		}
 		if (arr[i] % 2 == 0) {
 			sumOddElement += arr[i];
-    }
+		}
 	}
 	return sumOddElement - sumEvenElement;
 }
@@ -69,8 +70,8 @@ function averageEvenElementsWorker(...arr) {
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] % 2 == 0) {
 			sumEvenElement += arr[i];
-		countEvenElement++;
-    }
+			countEvenElement++;
+		}
 		average = +(sumEvenElement / countEvenElement).toFixed(2);
 	}
 	return average;
@@ -86,7 +87,12 @@ function makeWork(arrOfArr, func) {
 	}
 	return maxWorkerResult;
 }
-const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+const arr = [
+	[10, 10, 11, 20, 10],
+	[67, 10, 2, 39, 88],
+	[72, 75, 51, 87, 43],
+	[30, 41, 55, 96, 62]
+];
 console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
 console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
 console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
