278ad1a8922b0e395f4f91e0e5a475d9ff9f6913
AShcheblykina <116199504+AShcheblykina@users.noreply.github.com>
2023-05-07 22:30:57 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,15 +4,19 @@ function getArrayParams(...arr) {
 	let max = -Infinity;
 
 	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] < min) min = arr[i];
-		else if (arr[i] > max) max = arr[i];
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
 		sum += arr[i];
 	}
-const avg = (sum / arr.length).toFixed(2)
+	const avg = (sum / arr.length).toFixed(2)
 	return {
 		min: min,
 		max: max,
-		avg:  Number(avg)
+		avg: Number(avg)
 	};
 }
 
