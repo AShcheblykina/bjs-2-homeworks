2584813220c4cbc66ccc747a4313ddd82144a938
AShcheblykina <116199504+AShcheblykina@users.noreply.github.com>
2023-05-14 23:10:35 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,14 +8,14 @@ Student.prototype.setSubject = function (subjectName) {
 this.subject = subjectName;
 }
 Student.prototype.addMark = function (mark) {
-if (this.marks === 0) {
+if (this.marks === undefined) {
   this.marks = [mark];
 }  else {
   this.marks.push(mark)
 } 
 }
   Student.prototype.addMarks = function (...marks) {
-    if (this.marks === 0) {
+    if (this.marks === undefined) {
       this.marks = marks;
     } else {
        this.marks = this.marks.concat(marks);
@@ -33,3 +33,13 @@ if (this.marks === 0) {
     delete this.marks;
     this.excluded = reason;
   }
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
