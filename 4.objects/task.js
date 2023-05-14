e5dbc209e2b7418df66aba7d385cc625905ecb35
AShcheblykina <116199504+AShcheblykina@users.noreply.github.com>
2023-05-14 23:04:50 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
   
 }
-
 Student.prototype.setSubject = function (subjectName) {
-  
+this.subject = subjectName;
 }
-
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMark = function (mark) {
+if (this.marks == undefined) {
+  this.marks = [mark];
+}  else {
+  this.marks.push(mark)
+} 
 }
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks == undefined) {
+      this.marks = marks;
+    } else {
+       this.marks = this.marks.concat(marks);
+    }
+  }
+  Student.prototype.getAverage = function (getAverage) {
+    if (this.marks === undefined){
+      return 0;
+    } else {
+      return this.marks.reduce( ( a, v ) => a + v, 0 ) / this.marks.length;
+    }
+  }
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
