948ec1ffa7ba66ee7f5af65ce1da96862de98bf2
AShcheblykina <116199504+AShcheblykina@users.noreply.github.com>
2023-05-31 22:24:36 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,73 +1,62 @@
 function parseCount(avg) {
-	let mean = Number.parseFloat(avg);
-	if (Number.isNaN(mean)) {
-		throw new Error("Невалидное значение");
-	}
-	return mean;
-}
-     try {
-     console.log(parseCount());
-   } catch (error) {
-     console.log(error.message);
+  let mean = Number.parseFloat(avg);
+  if (Number.isNaN(mean)) {
+    throw new Error("Невалидное значение");
+  }
+  return mean;
 }
 
-
-function validateCount(average){
-    try {
-        console.log(parseCount(average));
-    } catch(error){
-      console.log("ОШИБКА: Введено не валидное значение!!!")  
-    }
-    
+function validateCount(average) {
+  try {
+    console.log(parseCount(average));
+  } catch (error) {
+    console.log("ОШИБКА: Введено невалидное значение!!!");
+  }
 }
 
-  validateCount("uihj")
+class Triangle {
+  constructor(leg_1, leg_2, hypotenuse) {
+    this._leg_1 = leg_1;
+    this._leg_2 = leg_2;
+    this._hypotenuse = hypotenuse;
+    this._perimeter = leg_1 + leg_2 + hypotenuse;
+    this._half = this._perimeter * 0.5;
+    this._area = Math.sqrt(
+      this._half *
+      (this._half - leg_1) *
+      (this._half - leg_2) *
+      (this._half - hypotenuse)
+    ).toFixed(3);
 
-
-class Triangle{
-    constructor(leg_1,leg_2,hypotenuse) {
-        this.leg_1 = leg_1;
-        this.leg_2 = leg_2;
-        this.hypotenuse = hypotenuse;
-        this.perimeter = leg_1 + leg_2 + hypotenuse;
-        this.half = this.perimeter * 0.5;
-        this.area = Math.sqrt(
-            this.half * 
-            (this.half - leg_1) * 
-            (this.half - leg_2) * 
-            (this.half - hypotenuse)).toFixed(3);
-    
     if (
-        leg_1 + leg_2 < hypotenuse || 
-        leg_1 + hypotenuse < leg_2 || 
-        leg_2 + hypotenuse < leg_1
+      leg_1 + leg_2 < hypotenuse ||
+      leg_1 + hypotenuse < leg_2 ||
+      leg_2 + hypotenuse < leg_1
     ) {
-        throw new Error("Треугольник с такими сторонами           не сущетсвует")
+      throw new Error("Треугольник с такими сторонами не существует");
     }
-    }
-      
-    get perimeter() {
-        return this._perimeter;    
-    }
-    get area() {
-        return this._area;
-    }
+  }
+
+  get perimeter() {
+    return this._perimeter;
+  }
+
+  get area() {
+    return this._area;
+  }
 }
 
+function getTriangle(leg_1, leg_2, hypotenuse) {
+  try {
+    let triangle = new Triangle(leg_1, leg_2, hypotenuse);
+    let perimeter = triangle.perimeter;
+    let area = triangle.area;
 
-
-function getTriangle(leg_1, leg_2, hypotenuse){
-    try {
-        let triangle = new Triangle(
-            leg_1, leg_2, hypotenuse);
-        let perimeter = triangle.perimeter;
-        let area = triangle.area;
-        
-        console.log("Периметр треугольника: ", perimeter);
-        console.log("Площадь треугольника: ", area);
-    } catch (error) {
-        console.log("ОШИБКА ОБРАБОТКИ ДАННЫХ");
-    }
+    console.log("Периметр треугольника: ", perimeter);
+    console.log("Площадь треугольника: ", area);
+  } catch (error) {
+    console.log("ОШИБКА ОБРАБОТКИ ДАННЫХ");
+  }
 }
 
-getTriangle(4,4,4);
+getTriangle(4, 4, 1);
